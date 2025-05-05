1e2e51a238c60741fe94b3f84df553be3c626e9e
Vladislav Kishkovskiy <67899905+aDeepRecession@users.noreply.github.com>
2025-05-05 14:30:11 +0300

    test: do not check public and mocks folders for test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,28 @@ export default defineConfig({
     test: {
         coverage: {
             reporter: ["text", "json", "html"],
+            exclude: [
+                "coverage/**",
+                "dist/**",
+                "**/node_modules/**",
+                "**/[.]**",
+                "packages/*/test?(s)/**",
+                "**/*.d.ts",
+                "**/virtual:*",
+                "**/__x00__*",
+                "**/\x00*",
+                "cypress/**",
+                "test?(s)/**",
+                "test?(-*).?(c|m)[jt]s?(x)",
+                "**/*{.,-}{test,spec,bench,benchmark}?(-d).?(c|m)[jt]s?(x)",
+                "**/__tests__/**",
+                "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*",
+                "**/vitest.{workspace,projects}.[jt]s?(on)",
+                "**/.{eslint,mocha,prettier}rc.{?(c|m)js,yml}",
+
+                "src/mocks/**",
+                "public/**",
+            ],
         },
         environment: "happy-dom",
     },
